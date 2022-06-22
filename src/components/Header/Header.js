580e9008edf969f25f28739d867fe69658af1a83
jmmon <jmmon93@gmail.com>
2022-06-22 09:57:22 -0400

    changing and adding header icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
 import styles from "./Header.module.css";
-import { ReactComponent as ClockIcon } from "assets/icons/clock.svg";
+import { ReactComponent as CalendarIcon } from "assets/icons/211703_calendar_icon.svg";
+// import { ReactComponent as ClockIcon } from "assets/icons/clock.svg";
+import calendar from "assets/icons/211703_calendar_icon.png";
 
 function Header() {
 	return (
 		<div className={styles.main}>
 			<div className={styles.container}>
 				<a className={`${styles.link} ${styles.special}`}href="#contact-link-target">
-					<ClockIcon className={styles.clock_icon} />
-					9-5 M-F
+					<CalendarIcon className={styles.calendar_icon} />
+					Schedule
 				</a>
 				<a className={styles.link} href="tel:15096907214">
 					(509) 690-7214
