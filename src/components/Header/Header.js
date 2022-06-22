88b976f4907e22bbf864676b250c4d942eee75b9
jmmon <jmmon93@gmail.com>
2022-06-22 10:25:41 -0400

    adjust header font, icons done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,32 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import styles from "./Header.module.css";
 import { ReactComponent as CalendarIcon } from "assets/icons/211703_calendar_icon.svg";
-// import { ReactComponent as ClockIcon } from "assets/icons/clock.svg";
-import calendar from "assets/icons/211703_calendar_icon.png";
+import { ReactComponent as PhoneIcon } from "assets/icons/211829_telephone_icon.svg";
+import { ReactComponent as ChatIcon } from "assets/icons/211711_chatbubble_icon.svg";
+import { ReactComponent as LocationIcon } from "assets/icons/211766_location_icon.svg";
 
 function Header() {
 	return (
 		<div className={styles.main}>
 			<div className={styles.container}>
-				<a className={`${styles.link} ${styles.special}`}href="#contact-link-target">
-					<CalendarIcon className={styles.calendar_icon} />
+				<Link
+					className={styles.link}
+					to="/order"
+				>
+					<CalendarIcon className={styles.icon} />
 					Schedule
-				</a>
+				</Link>
 				<a className={styles.link} href="tel:15096907214">
+					<PhoneIcon className={styles.icon} />
 					(509) 690-7214
 				</a>
-				<a
-					className={styles.link}
-					href="#contact-link-target"
-				>
+				<a className={styles.link} href="#contact-link-target">
+					<ChatIcon className={styles.icon} />
 					Email Us
 				</a>
-				<a
-					className={styles.link}
-					href="#map-link-target"
-				>
+				<a className={styles.link} href="#map-link-target">
+					<LocationIcon className={`${styles.icon} ${styles.location_icon}`} />
 					Northport, WA
 				</a>
 			</div>
