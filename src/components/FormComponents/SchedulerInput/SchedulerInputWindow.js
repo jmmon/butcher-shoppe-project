fbde5552122069eb8a14d3389a37397bc810e768
jmmon <jmmon93@gmail.com>
2022-06-29 20:07:43 -0400

    fully hooked into react-hook-form

--- src/components/FormComponents/SchedulerInput/SchedulerInputWindow.js
+++ src/components/FormComponents/SchedulerInput/SchedulerInputWindow.js
@@ -1,41 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
-import { useFormContext } from "react-hook-form";
-
 import "react-datepicker/dist/react-datepicker.css";
 import "./SchedulerInput.css";
+import { useFormContext } from "react-hook-form";
 
-const DateTimePicker = ({ 
-	title = '', 
+const DateTimePicker = ({
+	title = "",
 }) => {
 	const {
 		formState: { errors },
 		setValue,
 	} = useFormContext();
 
-	const name = "date.preferred";
+	const name = "date.alternate";
 
 	// let thisError = errors;
 	// name.split(".").forEach((key) => (thisError = thisError?.[key]));
 
 	const today = new Date();
-	const lastAvailableDate = new Date().setMonth(today.getMonth()+6);
+	const lastAvailableDate = new Date().setMonth(today.getMonth() + 6);
 
-	const [startDate, setStartDate] = useState(today);
-	
-	const isWeekday = (date) => {
-		const day = date.getDay(date);
-		return day !== 0;
+	const [startDate, setStartDate] = useState(null);
+	const [endDate, setEndDate] = useState(null);
+
+	// const isWeekday = (date) => {
+	// 	const day = date.getDay(date);
+	// 	return day !== 0;
+	// };
+
+	let timer = null;
+
+	const onChange = (dates) => {
+	  const [start, end] = dates;
+		setStartDate(start);
+		setEndDate(end);
+		setValue(name, {start, end});
 	};
-
-	const onChange = (date) => {
-		setStartDate(date);
-		setValue(name, date);
-	};
-
-	// useEffect(() => {
-	// 	register({name: "preferred", required: true})
-	// }, [register])
 
 	return (
 		<>
@@ -47,15 +47,19 @@ const DateTimePicker = ({
 				>
 					{title}
 				</label>
+
 				<DatePicker
 					className="date-time-picker scheduler--input"
 					name={name}
 					onChange={onChange}
 					// filterDate={isWeekday}
-					minDate={startDate}
+					startDate={startDate}
+					endDate={endDate}
+					minDate={today}
 					maxDate={lastAvailableDate}
 					showDisabledMonthNavigation
 					// dateFormat="MMMM d, yyyy"
+					selectsRange
 					inline
 				/>
 			</div>
--- src/components/FormComponents/Sections/AnimalsBasic/AnimalsBasic.js
+++ src/components/FormComponents/Sections/AnimalsBasic/AnimalsBasic.js
@@ -10,7 +10,7 @@ import PhoneInputForm from "../../InputForm/PhoneInputForm";
 import SelectForm from "components/FormComponents/SelectForm/SelectForm";
 import Button from "components/Button/Button";
 
-const DeleteButtonX = () => {
+const DeleteButtonX = ({onClick}) => {
 	return (
 		<button
 			style={{
@@ -21,20 +21,24 @@ const DeleteButtonX = () => {
 				height: "1rem",
 				cursor: "pointer",
 			}}
+			onClick={onClick}
 		>
 			X
 		</button>
 	);
 };
 
-const AnimalRow = ({ type, count, index }) => {
+const AnimalRow = ({ type, count, index, handleDelete }) => {
 	const [thisAnimalType, setThisAnimalType] = useState();
+
 	const handleSelectOther = (e) => {
 		const value = e.target.value;
 		setThisAnimalType(value);
 	};
+
 	return (
 		<div className={formStyles.field}>
+			<DeleteButtonX onClick={() => handleDelete(index)} />
 			<SelectForm
 				title="Type"
 				name={`animals.${index}.type`}
@@ -83,24 +87,40 @@ const AnimalRow = ({ type, count, index }) => {
 				/>
 			)}
 
-			<DeleteButtonX />
 		</div>
 	);
 };
 
 function AnimalsBasic() {
-	const storageFormObjectOrEmptyObject =
-		JSON.parse(window.localStorage.getItem("orderForm")) || {};
+	// const storageFormObjectOrEmptyObject =
+	// 	JSON.parse(window.localStorage.getItem("orderForm")) || {};
+
+	// const [addedAnimalsArray, setAddedAnimalsArray] = useState(
+	// 	storageFormObjectOrEmptyObject?.animals || [{ type: "beef", count: 1 }]
+	// );
 
 	const [addedAnimalsArray, setAddedAnimalsArray] = useState(
-		storageFormObjectOrEmptyObject?.animals || [{ type: "beef", count: 1 }]
+		[{ type: "beef", count: 1 }]
 	);
-	const handleAddAnimal = () => {
+
+	const handleAddAnimal = (e) => {
+		e.preventDefault();
+		console.log(e);
+		console.log('add animal');
 		setAddedAnimalsArray((previousAnimals) => [
 			...previousAnimals,
 			{ type: "beef", count: 1 },
 		]);
 	};
+
+	const handleDeleteAnimal = (index) => {
+		console.log(addedAnimalsArray);
+		if (index > 0) {
+			let newAnimals = [...addedAnimalsArray];
+			newAnimals.splice(index, 1);
+			setAddedAnimalsArray(newAnimals);
+		}
+	}
 
 	return (
 		<>
@@ -112,6 +132,7 @@ function AnimalsBasic() {
 						count={count}
 						index={index}
 						key={index}
+						handleDelete={handleDeleteAnimal}
 					/>
 				))}
 				<Button onClick={handleAddAnimal}>Add Another Animal</Button>
