826816fef65c0cca65fb6af341cb2c78c6b1d8f9
jmmon <jmmon93@gmail.com>
2022-07-01 08:08:50 -0400

    working on encryption functions to store order data securely

---
+++ backend/utils/encryption.js
@@ -0,0 +1,11 @@
+const crypto = require('crypto');
+const path = require('path');
+const fs = require('fs');
+const passphrase = "";
+
+
+
+// const {generateKeyPair} = require('crypto');
+// generateKeyPair('rsa', {
+// 	modulusLength: 5
+// })
