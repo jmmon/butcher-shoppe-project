8fca8e6d1a70a5202316376a40679a175fb22c4e
jmmon <jmmon93@gmail.com>
2022-07-01 13:42:02 -0400

    added order_archive email address, gets a copy of the order

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -29,8 +29,6 @@ router.route("/order").post((req, res) => {
 	const fullName = `${buyer.name.first} ${buyer.name.last}`;
 	const address = buyer.address;
 	const contact = {phone: buyer.phone_number, email: buyer.email_address};
-	// const animals = {}
-	// req.body.animals.forEach((animalData => animals[animalData.type] = animalData.count));
 	const animals = req.body.animals.map(animalData => `${animalData.type}: ${animalData.count}`);
 	const dates = req.body.dates;
 	const orderNumber = req.body.order_number;
@@ -69,42 +67,38 @@ router.route("/order").post((req, res) => {
 
 		const animalsString = animals.map(animalString => `\n${animalString.toUpperCase()}`);
 
-
-// `\n\n\n\n\n${googleLink}\n\n\n`
-
 		const animalsInfo = `\x20-\x20Animals scheduled for slaughter:\n${animalsString}\n`;
 
 
 		const datesInfo = `\x20-\x20Dates:\n\nPreferred Date: ${dates.preferred}\nAlternate Date${dates.alternate.start === dates.alternate.end ? `: ${dates.alternate.start}` : ` Window: \n\x20from: ${dates.alternate.start}\n\x20to: ${dates.alternate.end}`}`;
-		
+	
 
-		// const orderInfo = `${buyerInfoFormatted}\n\n${orderDates}`;
-
-
-		
 		const textBodyHeader_toUs = `You Received A New Order:\n`;
 		const textBodyFooter_toUs = `(Replies get sent to ${fullName} at ${contact.email})`;
 
 		const textBodyHeader_toThem = `We Have Received Your Order:\n`;
 		const textBodyFooter_toThem = `Have any questions? Reply to this email and we will get back to you!`;
 
-
-		// try {
-		// 	console.log("should send emails");
-		// } catch (e) {
-		// 	console.log("Error Sending email:", e);
-		// 	res.status(500).send("Server Error Submitting Order:", e);
-		// } finally {
-		// 	console.log("Finished sending both emails!");
-		// 	res.status(200).send("Order submitted!");
-		// }
-
 		Promise.all([
 			// to us
 			order_form__transporter.sendMail({
 				from: `"Orders @ The Butcher Shoppe" <${process.env.NOREPLY_EMAIL_USERNAME}>`,
-				// to: `"Northport Butcher Shoppe Info" <${process.env.INFO_EMAIL_USERNAME}>`,
-				to: process.env.NOREPLY_EMAIL_USERNAME,
+				to: `"Northport Butcher Shoppe Info" <${process.env.INFO_EMAIL_USERNAME}>`,
+				// to: process.env.NOREPLY_EMAIL_USERNAME,
+
+				replyTo: `"${fullName}" <${buyer.email_address}>`,
+
+				subject: `NEW ORDER from ${fullName} - Order #${orderNumber}`,
+
+				text: `${textBodyHeader_toUs}\n${contactInfo}\n\n\n${addressInfo}\n\n${googleMapsAddressString}\n\n\n${animalsInfo}\n\n${datesInfo}\n\n${textBodyFooter_toUs}`,
+
+				html: `<h2>${textBodyHeader_toUs}</h2><br>${contactInfo.replaceAll('\n', '<br>')}<br><br><br>${addressInfo.replaceAll('\n', '<br>')}<br><br><a href=${googleMapsAddressString}>Google Maps</a><br><br><br>${animalsInfo.replaceAll('\n', '<br>')}<br><br>${datesInfo.replaceAll('\n', '<br>')}<br><br><i>${textBodyFooter_toUs}</i>`,
+			}),
+
+			order_form__transporter.sendMail({
+				from: `"Orders @ The Butcher Shoppe" <${process.env.NOREPLY_EMAIL_USERNAME}>`,
+				to: `"Northport Butcher Shoppe Info" <${process.env.ORDER_ARCHIVE_EMAIL_USERNAME}>`,
+				// to: process.env.NOREPLY_EMAIL_USERNAME,
 
 				replyTo: `"${fullName}" <${buyer.email_address}>`,
 
@@ -117,7 +111,7 @@ router.route("/order").post((req, res) => {
 
 			// to them
 			order_form__transporter.sendMail({
-				from: `"Orders @ The Butcher Shoppe" <${process.env.NOREPLY_EMAIL_USERNAME}>`,
+				from: `"Orders - The Butcher Shoppe" <${process.env.NOREPLY_EMAIL_USERNAME}>`,
 
 				to: `"${fullName}" <${buyer.email_address}>`,
 
@@ -131,7 +125,7 @@ router.route("/order").post((req, res) => {
 			}),
 		])
 			.then(() => {
-				console.log("Finished sending both emails!");
+				console.log("Finished sending emails!");
 				res.status(200).send("Contact mail sent!");
 			})
 			.catch((e) => {
