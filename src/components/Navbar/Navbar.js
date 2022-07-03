d7c0b2373e98a0f879d70cc165ea829098668c2a
jmmon <jmmon93@gmail.com>
2022-07-03 11:23:18 -0400

    navbar converted; had to bring style into logo component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -39,106 +39,115 @@ function Navbar({ simple = false }) {
 
 	window.addEventListener("click", function (evt) {
 		if (!isMenuOpen) return;
+		// if (
+		// 	evt.target.matches(".nav-menu.active") ||
+		// 	evt.target.matches(".nav-item") ||
+		// 	evt.target.matches(".nav-links") ||
+		// 	evt.target.matches(".menu-toggle-button") ||
+		// 	evt.target.matches(".fas.fa-times")
+		// ) {
+		// 	return;
+		// }
 		if (
-			evt.target.matches(".nav-menu.active") ||
-			evt.target.matches(".nav-item") ||
-			evt.target.matches(".nav-links") ||
-			evt.target.matches(".menu-toggle-button") ||
+			evt.target.matches(`${styles.menu} ${styles.active}`) ||
+			evt.target.matches(styles.item) ||
+			evt.target.matches(styles.links) ||
+			evt.target.matches(styles.menu_toggle) ||
 			evt.target.matches(".fas.fa-times")
 		) {
 			return;
 		}
-
 
 		closeMobileMenu();
 	});
 
 	return (
 		<>
-			<nav className="navbar panel-shadow--light">
-				<div className="navbar-container">
+			<nav className={`${styles.main} panel-shadow--light`}>
+				<div className={styles.container}>
 					<Link
 						href="/"
-						className="navbar-home"
+						className={styles.home}
 						onClick={closeMobileMenu}
 					>
 						{/* The space below IS NECESSARY for the link to work! Remove it and the link disappears but the logo still shows */}{" "}
 						<LogoA1Component 
-						className="nav-logo" 
+						className={styles.logo}
 						fills={{
 							white: "#ccc", 
 							text: "#fff",
 						}}
+						styles={styles}
 						/>
 					</Link>
 
 					{/* Hamburger icon*/}
-					<div className="menu-toggle-button" onClick={ToggleMenu}>
-						<i className={isMenuOpen ? "fas fa-times" : "fas fa-bars"} />
+					<div className={styles.menu_toggle} onClick={ToggleMenu}>
+						<i className={isMenuOpen ? `fas fa-times ${styles.times}` : `fas fa-bars ${styles.bars}`} />
 					</div>
 
 					<ul
-						className={`${isMenuOpen ? "nav-menu active" : "nav-menu"}`}
+						className={`${isMenuOpen ? `${styles.menu} ${styles.active}` : `${styles.menu}`}`}
 					>
-						<li className="nav-item">
+						<li className={styles.item}>
 							<Link
 								href="/services"
-								className="nav-links"
+								className={styles.links}
 								onClick={closeMobileMenu}
 							>
 								Services
 							</Link>
 						</li>
-						<li className="nav-item">
+						<li className={styles.item}>
 							<Link
 								href="/meet-the-team"
-								className="nav-links"
+								className={styles.links}
 								onClick={closeMobileMenu}
 							>
 								Meet The Team
 							</Link>
 						</li>
-						<li className="nav-item">
+						<li className={styles.item}>
 							<Link
 								href="/how-to-order"
-								className="nav-links"
+								className={styles.links}
 								onClick={closeMobileMenu}
 							>
 								How To Order
 							</Link>
 						</li>
 
-						<li className="nav-item">
+						<li className={styles.item}>
 							<Link
 								href="/FAQ"
-								className="nav-links"
+								className={styles.links}
 								onClick={closeMobileMenu}
 							>
 								FAQ
 							</Link>
 						</li>
-						<li className="nav-item">
+						<li className={styles.item}>
 							<Link
 								href="/order"
-								className="nav-links"
+								className={styles.links}
 								onClick={closeMobileMenu}
 							>
 								Order
 							</Link>
 						</li>
-						<li className="nav-item">
+						<li className={styles.item}>
 							<a
 								href="#more-links"
-								className="nav-links"
+								className={styles.links}
 								onClick={closeMobileMenu}
 							>
 								More...
 							</a>
 						</li>
-						<li className="nav-item">
+						<li className={styles.item}>
 							<Link
 								href="/newsletter/subscribe"
-								className="nav-links mobile"
+								className={`${styles.links} ${styles.mobile}`}
 								onClick={closeMobileMenu}
 							>
 							Newsletter
@@ -150,9 +159,9 @@ function Navbar({ simple = false }) {
 					{isLargeScreen && (
 						<Link
 							href="/newsletter/subscribe"
-							className="btn-mobile newsletter-btn"
+							className={`btn-mobile ${styles.newsletter_btn}`}
 						>
-							<div className="btn btn--outline btn--large">
+							<div className={`${styles.newsletter_btn_inner} btn--outline btn--large`}>
 								Newsletter
 							</div>
 						</Link>
