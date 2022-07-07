50ecd43dfa98a05c7be1f42a7532d699ba898fbc
jmmon <jmmon93@gmail.com>
2022-07-07 10:23:25 -0400

    lazy load pages other than home and 404

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import 'preact/debug'
 import { Route, Switch } from "wouter";
 import "./App.css";
@@ -9,16 +9,31 @@ import Navbar from "components/Navbar/Navbar.js";
 import Footer from "components/Footer/Footer.js";
 
 import Home from "pages/Home/Home.js";
-import Services from "pages/Services/Services.js";
-import Faq from "pages/Faq/Faq.js";
-import Order from "pages/Order/Order.js";
-import HowToOrder from "pages/HowToOrder/HowToOrder.js";
-import Newsletter from "pages/Newsletter/Newsletter";
-import Unsubscribe from "pages/Newsletter/Unsubscribe";
-import SubscribeConfirm from "pages/Newsletter/Confirm/SubscribeConfirm";
-import MeetTheTeam from "pages/MeetTheTeam/MeetTheTeam";
-import Membership from "pages/Membership/Membership";
 import NotFound from "pages/NotFound/NotFound";
+
+const Services = lazy(()=> import("pages/Services/Services.js"));
+const Faq = lazy(()=> import("pages/Faq/Faq.js"));
+const Order = lazy(()=> import("pages/Order/Order.js"));
+const HowToOrder = lazy(()=> import("pages/HowToOrder/HowToOrder.js"));
+const Newsletter = lazy(()=> import("pages/Newsletter/Newsletter"));
+const Unsubscribe = lazy(()=> import("pages/Newsletter/Unsubscribe"));
+const SubscribeConfirm = lazy(()=> import("pages/Newsletter/Confirm/SubscribeConfirm"));
+const MeetTheTeam = lazy(()=> import("pages/MeetTheTeam/MeetTheTeam"));
+const Membership = lazy(()=> import("pages/Membership/Membership"));
+
+// const NotFound = lazy(()=> import("pages/NotFound/NotFound"));
+
+
+
+// import Services from "pages/Services/Services.js";
+// import Faq from "pages/Faq/Faq.js";
+// import Order from "pages/Order/Order.js";
+// import HowToOrder from "pages/HowToOrder/HowToOrder.js";
+// import Newsletter from "pages/Newsletter/Newsletter";
+// import Unsubscribe from "pages/Newsletter/Unsubscribe";
+// import SubscribeConfirm from "pages/Newsletter/Confirm/SubscribeConfirm";
+// import MeetTheTeam from "pages/MeetTheTeam/MeetTheTeam";
+// import Membership from "pages/Membership/Membership";
 
 
 function App() {
@@ -29,54 +44,102 @@ function App() {
 				<Navbar />
 				<div className="website-content-container">
 					<Switch>
+						<Route path="/">
+							<Home />
+						</Route>
+
 						<Route path="/newsletter/subscribe">
-							<Newsletter />
+							<Suspense fallback={<div>Loading...</div>}>
+								<Newsletter />
+
+							</Suspense>
 						</Route>
+
 						<Route path="/newsletter/unsubscribe">
+							<Suspense fallback={<div>Loading...</div>}>
 							<Unsubscribe />
+
+							</Suspense>
 						</Route>
+
 						<Route path="/newsletter/subscribe/confirm/:id">
 							{(params) => (
+								<Suspense fallback={<div>Loading...</div>}>
 								<SubscribeConfirm
 									isSubscribePage
 									id={params.id}
 								/>
+	
+								</Suspense>
+								
 							)}
 						</Route>
+
 						<Route path="/newsletter/unsubscribe/confirm/:id">
 							{(params) => (
+								<Suspense fallback={<div>Loading...</div>}>
 								<SubscribeConfirm
 									isSubscribePage={false}
 									id={params.id}
 								/>
+	
+								</Suspense>
+								
 							)}
 						</Route>
 
-						<Route path="/">
-							<Home />
-						</Route>
 						<Route path="/services">
+						<Suspense fallback={<div>Loading...</div>}>
+								
+	
 							<Services />
+								</Suspense>
 						</Route>
+
 						<Route path="/faq">
+						<Suspense fallback={<div>Loading...</div>}>
+								
 							<Faq />
+	
+									</Suspense>
 						</Route>
+
 						<Route path="/how-to-order">
+						<Suspense fallback={<div>Loading...</div>}>
 							<HowToOrder />
+								
+		
+										</Suspense>
 						</Route>
+
 						<Route path="/order">
+						<Suspense fallback={<div>Loading...</div>}>
 							<Order />
+								
+		
+										</Suspense>
 						</Route>
+
 						<Route path="/meet-the-team">
+						<Suspense fallback={<div>Loading...</div>}>
 							<MeetTheTeam />
+								
+		
+										</Suspense>
 						</Route>
+
 						<Route path="/membership">
+						<Suspense fallback={<div>Loading...</div>}>
 							<Membership />
+								
+		
+										</Suspense>
 						</Route>
 
 						<Route>
 							<NotFound />
 						</Route>
+
 					</Switch>
 					<Footer />
 				</div>
