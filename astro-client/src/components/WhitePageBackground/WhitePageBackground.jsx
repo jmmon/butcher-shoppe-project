4f88f5d216fd5a85769dd079fd54e1383ae44c2b
jmmon <jmmon93@gmail.com>
2022-07-08 10:55:53 -0400

    astro services page done and functioning

---
+++ astro-client/src/components/WhitePageBackground/WhitePageBackground.jsx
@@ -0,0 +1,16 @@
+// import React from "react";
+import styles from "./WhitePageBackground.module.css";
+
+function WhitePageBackground({ separate = false, className = "", children }) {
+	return (
+		<section
+			className={`flex-col-jcenter panel-shadow--dark ${className} ${styles.container} ${
+				separate && styles.separate
+			}`}
+		>
+			{children}
+		</section>
+	);
+}
+
+export default WhitePageBackground;
