0125c3c7c303253c06b9fc362fc92b63f08b8035
jmmon <jmmon93@gmail.com>
2022-07-11 11:32:33 -0400

    more image optimizations (homepage)

--- src/components/HomepageTitle/Images.js
+++ src/components/HomepageTitle/Images.js
@@ -5,11 +5,11 @@ const Images = [
 	// 	size: "contain",
 	// },
 	{
-		url: require("assets/images/homepage/current/_DSC1246.jpg"),
+		url: require("assets/images/homepage/current/_DSC1246-q80-c4.jpg"),
 		position: "50% 58%",
 	},
 	{
-		url: require("assets/images/homepage/current/_MG_1051-1500-1000-q50-c4.jpg"),
+		url: require("assets/images/homepage/current/_MG_1051-1024x683_q60_c4.jpg"),
 		position: "50% 45%",
 	},
 	{
@@ -17,7 +17,7 @@ const Images = [
 		position: "50% 60%",
 	},
 	{
-		url: require("assets/images/homepage/current/image-1-117-q80-c4.jpg"),
+		url: require("assets/images/homepage/current/image-1-117-q55-c4.jpg"),
 		position: "50% 40%",
 	},
 ];
@@ -32,19 +32,29 @@ want to use small + med + large images for progressive loading
 Goal: parabolic filesize differences.
 
 Current sizes:
-[0]: 130kb
-[1]: 8.48mb!!!
-[2]: 89.2kb
-[3]: 244kb
+[0]: 131kb
+[1]: 8686kb!!!
+[2]: 90kb
+[3]: 245kb
 
-So bring [1] down to ~220kb or so first. That will be the max size.
+9,152 kb total
 
-Then LARGE photos will be 89.2kb ~ 244kb
-So MEDIUM should be 9.44kb ~ 15.62kb ? 
-So SMALL should be 3.07kb ~ 3.95kb ? 
+New Sizes:
+[0]: 131kb
+[1]: 143kb
+[2]: 90kb
+[3]: 143kb
 
-Additionally provide alternate photos for mobile phones?
- - Crop them for portrait? Or probably not, just in case they switch to landscape.
+507 kb total!
+Still could reduce some smaller probably
+
+New Sizes:
+[0]: 60kb
+[1]: 85kb
+[2]: 90kb
+[3]: 88kb
+
+323 kb total! (over 28x smaller than original; 1.5x smaller than last set)
 
 
 
