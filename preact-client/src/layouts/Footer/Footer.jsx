8652722522eefc28181c745bf027e576b31ea42b
jmmon <jmmon93@gmail.com>
2022-08-02 10:50:08 -0400

    new logo? fixed footer bottom text centering; added emails on bottom of page

--- preact-client/src/layouts/Footer/Footer.jsx
+++ preact-client/src/layouts/Footer/Footer.jsx
@@ -4,7 +4,9 @@ import Styles from "./Footer.module.css";
 import Subscribe from "components/Subscribe/Subscribe";
 import FooterSuspenseContainer from "../../components/FooterSuspenseContainer/FooterSuspenseContainer";
 import LinkScrollUp from "components/LinkScrollUp/LinkScrollUp";
-import LogoComponent from "assets/logo/LogoComponent";
+// import LogoComponent from "assets/logo/LogoComponent";
+import LogoComponent from "assets/logo/LogoComponent_final";
+
 import TelLink from "components/TelLink/TelLink";
 
 const facebookUrl = "https://www.facebook.com/TheButcherShoppe2022";
@@ -40,7 +42,7 @@ function Footer() {
 
 	return (
 		<footer
-			className={`flex-col-jcenter panel-shadow--dark ${Styles.container}`}
+			className={`flex-col-jcenter panel-shadow--dark ${Styles.footer}`}
 			id="footer-container"
 		>
 			<section className={`flex-col-jcenter-acenter ${Styles.section}`}>
@@ -119,21 +121,6 @@ function Footer() {
 					</div>
 				</div>
 				<div className={`${Styles.links_half} flex-jbetween`}>
-					{/* <div className={styles.links_column}>
-						<div className={`flex-col ${styles.link_items}`}>
-							<h2 className={`${styles.heading}`}>
-								Social Media
-							</h2>
-							<a
-								href={facebookUrl}
-								target="_blank"
-								rel="noreferrer"
-								className={`main-link `}
-							>
-								Facebook
-							</a>
-						</div>
-					</div> */}
 					<div className={Styles.links_column}>
 						<h2 className={`${Styles.heading}`}>Keep In Touch</h2>
 						<div className={`flex-col ${Styles.link_items}`}>
@@ -153,6 +140,7 @@ function Footer() {
 							</a>
 						</div>
 					</div>
+					
 				</div>
 			</section>
 
@@ -164,58 +152,69 @@ function Footer() {
 				</div>
 			</section>
 
-			{/* <section className={`flex-jcenter-acenter ${styles.section} ${styles.logo_surround}`}> */}
-			<section className={`flex-jcenter-acenter ${Styles.section}`}>
-				{/* <div className={styles.logo_surround}> */}
-				<LogoComponent
-					fills={{
-						white: "#ccc",
-					}}
-					// fills={{
-					// 	brown: "#765", //handle
-					// 	black: "#aaa", //blade, trees
-					// 	white: "#242424", //outside, mountain-trace, holes
-					// 	// grey: "#888", // mountains
-					// 	//red //text
-					// }}
-					className={Styles.banner_2}
-				/>
-				{/* </div> */}
-				{/* <LogoComponent fill="white" className={styles.banner} /> */}
-			</section>
+			<div className={Styles.dark_wrapper}>
 
-			<section className={`flex-col-acenter ${Styles.social_media}`}>
-				<div
-					className={`flex-jaround-acenter ${Styles.social_media_wrap}`}
-				>
-					<LinkScrollUp
-						path="/"
-						className={`flex ${Styles.social_name}`}
-					>
-						The Butcher Shoppe
-					</LinkScrollUp>
+				<div className={`flex-col-jcenter-acenter ${Styles.bottom_lighter_container}`}> 
+					<section className={`flex-jcenter-acenter ${Styles.logo_wrapper}`}>
+						<LogoComponent  className={Styles.banner_2}/>
+					</section>
 
-					<div
-						className={`flex-acenter-jcenter gap-1 ${Styles.social_icons}`}
-					>
-						<h4>Find Us On:</h4>
-						<a
-							className={`main-link ${Styles.social_small_font}`}
-							href={facebookUrl}
-							target="_blank"
-							rel="noreferrer"
-							aria-label="Facebook"
+
+					<section className={`flex-jcenter-acenter ${Styles.social_media}`}>
+							<LinkScrollUp
+								path="/"
+								className={`flex-jcenter ${Styles.social_name}`}
+							>
+								The Butcher Shoppe
+							</LinkScrollUp>
+
+							<div
+								className={`flex-acenter-jcenter gap-1 ${Styles.social_icons}`}
+							>
+								<h4>Find Us On:</h4>
+								<a
+									className={`main-link ${Styles.social_small_font}`}
+									href={facebookUrl}
+									target="_blank"
+									rel="noreferrer"
+									aria-label="Facebook"
+								>
+									<i className="fab fa-facebook-f"></i>
+								</a>
+							</div>
+					</section>
+
+					<section className={`flex-col-jcenter-acenter ${Styles.social_emails_wrapper}`}>
+						<div className={`flex-jcenter-acenter-wrap ${Styles.social_emails_container}`}
 						>
-							<i className="fab fa-facebook-f"></i>
-						</a>
-					</div>
+							<span className={`${Styles.social_emails_name}`}>Questions?</span>
+							<a 
+							href="mailto: info@thenorthportbutchershoppe.com"
+							className={`${Styles.social_emails}`}
+							>
+								info@thenorthportbutchershoppe.com
+							</a>
+						</div>
+						<div
+							className={`flex-jcenter-acenter-wrap ${Styles.social_emails_container}`}
+							>
+								<span className={`${Styles.social_emails_name}`}>Website Help?</span>
+							
+							<a href="mailto: support@thenorthportbutchershoppe.com"
+							className={`${Styles.social_emails}`}>
+								support@thenorthportbutchershoppe.com
+							</a>
+						</div>
+					</section>
+
+
 					<small
-						className={`${Styles.website_rights} ${Styles.social_small_font}`}
+						className={Styles.website_rights}
 					>
 						C 2022
 					</small>
 				</div>
-			</section>
+			</div>
 		</footer>
 	);
 }
