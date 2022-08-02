0a4ed1a8875f3e8ca623dbe3443eb2d49fd50180
jmmon <jmmon93@gmail.com>
2022-08-02 12:49:13 -0400

    new logo on navbar looks alright!

--- preact-client/src/layouts/Navbar/Navbar.jsx
+++ preact-client/src/layouts/Navbar/Navbar.jsx
@@ -11,8 +11,11 @@ function Navbar() {
 	return (
 		<nav className={`${Styles.wrapper} panel-shadow--light`}>
 			<div className={Styles.navbar}>
+					<div className={Styles.logo_bg_ellipse}></div>
 				<Link href="/" className={Styles.home}>
-					{/* The space below IS NECESSARY for the link to work! Remove it and the link disappears but the logo still shows */}{" "}
+					{/* The space below IS NECESSARY for the link to work! Remove it and the link disappears but the logo still shows */}
+					{" "}
+					{/* <div className={Styles.logo_bg_square}></div> */}
 					<LogoComponent
 						className={Styles.logo}
 						fills={{
