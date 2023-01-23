4948372916f07dee22cfeaf62fc016a92d9d2947
jmmon <jmmon93@gmail.com>
2023-01-23 06:27:45 -0800

    firstWords styling for cards, meet the team

--- preact-client/src/components/Card/Card.jsx
+++ preact-client/src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Styles from "./Card.module.css";
+import React from 'react';
+import Styles from './Card.module.css';
 
-function ImageHeading({ bg = null, title = "" }) {
+function ImageHeading({ bg = null, title = '' }) {
 	return (
 		<div
 			className={`${Styles.image_heading} padding-2--vertical`}
@@ -11,45 +11,76 @@ function ImageHeading({ bg = null, title = "" }) {
 				}
 			}
 		>
-			<h3
-				className={`${bg && "text-white"} ${
-					Styles.title
-				} text-shadow--lg`}
-			>
-				{title}
-			</h3>
+			<h3 className={`${bg && 'text-white'} ${Styles.title} text-shadow--lg`}>{title}</h3>
 		</div>
 	);
 }
 
+function splitWords(
+	p,
+	{ firstLargeWords, largeWordsSize } = { firstLargeWords: 0, largeWordsSize: 0 }
+) {
+	if (firstLargeWords <= 0) {
+		return { first: null, rest };
+	}
+
+	const split = p.split(' ');
+	const firstWords = split.slice(0, firstLargeWords).join(' ');
+	const rest = split.slice(firstLargeWords).join(' ');
+
+	return { first: firstWords, rest };
+}
+
 export default function Card({
-	className = "",
-	title = "",
+	className = '',
+	title = '',
 	subtitle = null,
 	paragraphs = null,
 	bg = null,
 	children = null,
+	options = null,
 }) {
-	const content = paragraphs?.map((item) => (
-		<p key={paragraphs.indexOf(item)}>{item}</p>
-	));
+	const formattedParagraphs = paragraphs?.map((p, index) => {
+		if (!options || options === null) {
+			return <p key={index}>{p}</p>;
+		}
+
+		const { first, rest } = splitWords(p, options[index]);
+		return (
+			<p key={index}>
+				<span
+					style={{
+						fontSize: options[index].largeWordsSize || '100%',
+						fontWeight: 'bold',
+						lineHeight: 1.3,
+						fontStyle: options[index].fontStyle || 'normal',
+						textDecoration: options[index].textDecoration || 'none',
+					}}
+				>
+					{first}{' '}
+				</span>
+				{rest}
+			</p>
+		);
+	});
 
 	return (
-		<section
-			className={`${Styles.container} card--width  card--font-size ${className}`}
-		>
-			{bg && <ImageHeading bg={bg} title={title} />}
+		<section className={`${Styles.container} card--width  card--font-size ${className}`}>
+			{bg && (
+				<ImageHeading
+					bg={bg}
+					title={title}
+				/>
+			)}
 			<div className={`${Styles.card_content} card--content-width`}>
-				{!bg && title && (
-					<h2 className={`${Styles.title} text-center`}>{title}</h2>
-				)}
+				{!bg && title && <h2 className={`${Styles.title} text-center`}>{title}</h2>}
 				{subtitle && (
 					<h3 className={`${Styles.subtitle} text-center`}>
 						<i>{subtitle}</i>
 					</h3>
 				)}
 
-				{content && content}
+				{formattedParagraphs && formattedParagraphs}
 				{children}
 			</div>
 		</section>
--- preact-client/src/pages/MeetTheTeam/MeetTheTeam.jsx
+++ preact-client/src/pages/MeetTheTeam/MeetTheTeam.jsx
@@ -1,43 +1,52 @@
-import React from "react";
-import Styles from "./MeetTheTeam.module.css";
+import React from 'react';
+import Styles from './MeetTheTeam.module.css';
 
-import bgImage from "assets/images/image-1-113-cropped-55.jpg";
-import PageTitle from "layouts/PageTitle/PageTitle";
+import bgImage from 'assets/images/image-1-113-cropped-55.jpg';
+import PageTitle from 'layouts/PageTitle/PageTitle';
 
-import Card from "components/Card/Card";
-import PageLayout from "layouts/PageLayout/PageLayout";
-import { Helmet } from "react-helmet";
+import Card from 'components/Card/Card';
+import PageLayout from 'layouts/PageLayout/PageLayout';
+import { Helmet } from 'react-helmet';
 
-export default function MeetTheTeam({path, setVisitedRoutes}) {
+export default function MeetTheTeam({ path, setVisitedRoutes }) {
 	React.useEffect(() => {
-		setVisitedRoutes((prev) => ([...prev, path]));
-	},[])
+		setVisitedRoutes((prev) => [...prev, path]);
+	}, []);
 	return (
 		<PageLayout
 			helmet={
 				<Helmet>
-					<title>
-						Meet The Team | The Butcher Shoppe | Northport, WA
-					</title>
+					<title>Meet The Team | The Butcher Shoppe | Northport, WA</title>
 					<meta
 						name="description"
 						content="Our team is proud to serve the greater Northport and Colville area with our mobile slaughter - and soon, meat processing."
 					/>
 				</Helmet>
 			}
-			title={<PageTitle title="Meet The Team" bgImage={bgImage} />}
+			title={
+				<PageTitle
+					title="Meet The Team"
+					bgImage={bgImage}
+				/>
+			}
 		>
+			<div className={Styles.container}>
+
 			<Card
 				title="The Team"
 				paragraphs={[
-					"The vision and talent behind The Butcher Shoppe can be found in the hearts of 2 Stevens County families that partnered up to build a business that would serve their local community. The need for meat processing has been increasing in our end of the county as more of our neighbors want to control their own food production. Both our Mobile Farm Kill Truck and The Butcher Shoppe are intended to provide service that will help you meet your goals as a grower and a buyer.",
-					"Trent and Tamara Smith are longtime residents of Stevens	County. They have had the pleasure of raising their 5	children, a few pigs and a couple dogs, in one of the most beautiful places on earth for almost 20 years now! They are motivated by a desire to serve their community through providing a service they actually enjoy! They believe that their business will bring new jobs and more industry to their small town that is full of potential. The future belongs to those who prepare for it and they want to build something that will sustain the following generations.",
+					'The vision and talent behind The Butcher Shoppe can be found in the hearts of 2 Stevens County families that partnered up to build a business that would serve their local community. The need for meat processing has been increasing in our end of the county as more of our neighbors want to control their own food production. Both our Mobile Farm Kill Truck and The Butcher Shoppe are intended to provide service that will help you meet your goals as a grower and a buyer.',
 				]}
-			>
-				<p>
-					
-				</p>
-			</Card>
+				options={[{ firstLargeWords: 4, largeWordsSize: '150%', fontStyle: 'none' , textDecoration: 'none'}]}
+			/>
+			<Card
+				paragraphs={[
+					'Trent and Tamara Smith are longtime residents of Stevens	County. They have had the pleasure of raising their 5	children, a few pigs and a couple dogs, in one of the most beautiful places on earth for almost 20 years now! They are motivated by a desire to serve their community through providing a service they actually enjoy! They believe that their business will bring new jobs and more industry to their small town that is full of potential. The future belongs to those who prepare for it and they want to build something that will sustain the following generations.',
+				]}
+				options={[{ firstLargeWords: 4, largeWordsSize: '150%', fontStyle: 'none', textDecoration: 'none' }]}
+			/>
+
+			</div>
 
 			{/* <section className="team-container">
 					<img src={topImage} className="team-img-left panel-shadow--dark" />
