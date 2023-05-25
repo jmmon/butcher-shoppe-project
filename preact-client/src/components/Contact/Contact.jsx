adea8b3cabd2e70c81de04fc07f175724fe972e5
jmmon <jmmon93@gmail.com>
2023-05-25 10:34:02 -0700

    contact box constants messages

--- preact-client/src/components/Contact/Contact.jsx
+++ preact-client/src/components/Contact/Contact.jsx
@@ -1,241 +1,265 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import Styles from "./Contact.module.css";
+import Styles from './Contact.module.css';
 
 const contactBoxBackendUri =
-	"https://thenorthportbutchershoppe.com/server/contact";
+  'https://thenorthportbutchershoppe.com/server/contact';
 
 // const contactBoxBackendUri =
 // 	"https://thenorthportbutchershoppe.com/staging/server/contact";
 
 // const contactBoxBackendUri = "localhost:3001/server/contact";
 
-const headers = { "Content-Type": "application/json" };
+const headers = { 'Content-Type': 'application/json' };
+
+const RESPONSE_MSG = {
+  DEFAULT: '',
+  PENDING: '',
+  SUCCESS: 'Message sent! A copy will be sent to your provided email address.',
+  ERROR: 'Something went wrong. Please try again later.',
+};
+
+const SEND_BUTTON_MSG = {
+  DEFAULT: 'Send', // default
+  PENDING: 'Sending...',
+  SUCCESS: 'Sent!',
+  ERROR: 'Oops, try again!',
+};
 
 const Contact = () => {
-	const [input, setInput] = useState({
-		contact__name: "",
-		contact__email: "",
-		contact__topic: "general",
-		contact__textarea: "",
-	});
+  const [input, setInput] = useState({
+    contact__name: '',
+    contact__email: '',
+    contact__topic: 'general',
+    contact__textarea: '',
+  });
 
-	const [responseFromContactBox, setResponseFromContactBox] = useState({
-		data: null,
-		error: null,
-		isLoading: false,
-	});
+  const [responseFromContactBox, setResponseFromContactBox] = useState({
+    data: null,
+    error: null,
+    isLoading: false,
+  });
 
-	let timer = null;
+  let timer = null;
 
-	const inputHandleChange = (e) => {
-		e.persist();
-		//console.log(e, e.target, e.target.name);
-		//console.log(e.target.value);
-		setInput((prevInput) => {
-			return {
-				...prevInput,
-				[e.target.name]: e.target.value,
-			};
-		});
-	};
+  const inputHandleChange = (e) => {
+    e.persist();
+    //console.log(e, e.target, e.target.name);
+    //console.log(e.target.value);
+    setInput((prevInput) => {
+      return {
+        ...prevInput,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
 
-	const resetButtonWithSetTimeout = () => {
-		timer = setTimeout(() => {
-			// reset button timer
-			setResponseFromContactBox((prevState) => ({
-				...prevState,
-				data: null,
-				error: null,
-				isLoading: false,
-			}));
-		}, 6000);
-	};
+  // starts the reset timer
+  const resetButtonWithSetTimeout = () => {
+    timer = setTimeout(() => {
+      setResponseFromContactBox((prevState) => ({
+        ...prevState,
+        data: null,
+        error: null,
+        isLoading: false,
+      }));
+    }, 6000);
+  };
 
-	useEffect(() => {
-		// componentWillUnmount
-		return () => {
-			if (timer) {
-				clearTimeout(timer);
-			}
-		};
-	});
+  // clear timeout on dismount
+  useEffect(() => {
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  });
 
-	const formHandleSubmit = (e) => {
-		e.preventDefault();
+  const formHandleSubmit = (e) => {
+    e.preventDefault();
 
-		setResponseFromContactBox((prevState) => ({
-			...prevState,
-			isLoading: true,
-			data: null,
-			error: null,
-		}));
+    setResponseFromContactBox((prevState) => ({
+      ...prevState,
+      isLoading: true,
+      data: null,
+      error: null,
+    }));
 
-		const dataFromContactBox = {
-			contact__name: input.contact__name,
-			contact__email: input.contact__email,
-			contact__topic: input.contact__topic,
-			contact__textarea: input.contact__textarea,
-			contact__number: (Math.random() * 100000) | 0,
-		};
+    const dataFromContactBox = {
+      contact__name: input.contact__name,
+      contact__email: input.contact__email,
+      contact__topic: input.contact__topic,
+      contact__textarea: input.contact__textarea,
+      contact__number: (Math.random() * 100000) | 0,
+    };
 
-		// console.log("message data:", dataFromContactBox);
-		// console.log("attempt axios post to:", contactBoxBackendUri);
+    // console.log("message data:", dataFromContactBox);
+    // console.log("attempt axios post to:", contactBoxBackendUri);
 
-		axios
-			.post(contactBoxBackendUri, dataFromContactBox, {
-				headers: headers,
-			})
-			.then((res) => {
-				console.log("response status:", res.status);
+    axios
+      .post(contactBoxBackendUri, dataFromContactBox, {
+        headers: headers,
+      })
+      .then((res) => {
+        console.log('response status:', res.status);
 
-				setResponseFromContactBox((prevState) => ({
-					...prevState,
-					data: res.data,
-					error: null,
-					isLoading: false,
-				}));
+        setResponseFromContactBox((prevState) => ({
+          ...prevState,
+          data: res.data,
+          error: null,
+          isLoading: false,
+        }));
 
-				// clear input boxes
-				setInput((prevInput) => {
-					return {
-						...prevInput,
-						contact__name: "",
-						contact__email: "",
-						contact__topic: "general",
-						contact__textarea: "",
-					};
-				});
-			})
-			.catch((e) => {
-				console.log("axios post error:", e);
+        // clear input boxes
+        setInput((prevInput) => {
+          return {
+            ...prevInput,
+            contact__name: '',
+            contact__email: '',
+            contact__topic: 'general',
+            contact__textarea: '',
+          };
+        });
+      })
+      .catch((e) => {
+        console.log('axios post error:', e);
 
-				setResponseFromContactBox((prevState) => ({
-					...prevState,
-					data: null,
-					error: e,
-					isLoading: false,
-				}));
-			})
-			.finally(() => {
-				resetButtonWithSetTimeout();
-			});
-	};
+        setResponseFromContactBox((prevState) => ({
+          ...prevState,
+          data: null,
+          error: e,
+          isLoading: false,
+        }));
+      })
+      .finally(() => {
+        resetButtonWithSetTimeout();
+      });
+  };
 
-	return (
-		<form className={Styles.form} onSubmit={formHandleSubmit}>
-			<div className={Styles.double_input}>
-				<div className={`${Styles.container} ${Styles.small}`}>
-					<label className={Styles.label} htmlFor="contact__name">
-						Name:
-					</label>
+  return (
+    <form
+      className={Styles.form}
+      onSubmit={formHandleSubmit}
+    >
+      <div className={Styles.double_input}>
+        <div className={`${Styles.container} ${Styles.small}`}>
+          <label
+            className={Styles.label}
+            htmlFor='contact__name'
+          >
+            Name:
+          </label>
 
-					<input
-						className={Styles.input}
-						type="text"
-						name="contact__name"
-						placeholder="Your name"
-						value={input.contact__name}
-						onChange={inputHandleChange}
-						required
-					/>
-				</div>
+          <input
+            className={Styles.input}
+            type='text'
+            name='contact__name'
+            placeholder='Your name'
+            value={input.contact__name}
+            onChange={inputHandleChange}
+            required
+          />
+        </div>
 
-				<div className={`${Styles.container} ${Styles.small}`}>
-					<label className={Styles.label} htmlFor="contact__email">
-						Email:
-					</label>
+        <div className={`${Styles.container} ${Styles.small}`}>
+          <label
+            className={Styles.label}
+            htmlFor='contact__email'
+          >
+            Email:
+          </label>
 
-					<input
-						className={Styles.input}
-						type="email"
-						name="contact__email"
-						placeholder="Your email"
-						value={input.contact__email}
-						onChange={inputHandleChange}
-						required
-					/>
-				</div>
-			</div>
+          <input
+            className={Styles.input}
+            type='email'
+            name='contact__email'
+            placeholder='Your email'
+            value={input.contact__email}
+            onChange={inputHandleChange}
+            required
+          />
+        </div>
+      </div>
 
-			<div className={`${Styles.container} ${Styles.large}`}>
-				<label
-					className={`${Styles.label} ${Styles.topic}`}
-					htmlFor="contact__topic"
-				>
-					Topic:
-				</label>
+      <div className={`${Styles.container} ${Styles.large}`}>
+        <label
+          className={`${Styles.label} ${Styles.topic}`}
+          htmlFor='contact__topic'
+        >
+          Topic:
+        </label>
 
-				<select
-					className={`${Styles.input} ${Styles.topic}`}
-					name="contact__topic"
-					id="topic"
-					onChange={inputHandleChange}
-					value={input.contact__topic}
-				>
-					<option value="general">General Question</option>
-          {/*
-          <option value="pricing">Pricing Question</option>
-          <option value="orders">Orders Question</option>
-          */}
-					<option value="support">Website Support</option>
-				</select>
-			</div>
+        <select
+          className={`${Styles.input} ${Styles.topic}`}
+          name='contact__topic'
+          id='topic'
+          onChange={inputHandleChange}
+          value={input.contact__topic}
+        >
+          <option value='general'>General Question</option>
+          <option value='support'>Website Support</option>
+        </select>
+      </div>
 
-			<div 
-			className={`${Styles.container} ${Styles.large} ${Styles.grow}`}
-			>
-				<label className={Styles.label} htmlFor="contact__textarea">
-					How can we help you?
-				</label>
+      <div className={`${Styles.container} ${Styles.large} ${Styles.grow}`}>
+        <label
+          className={Styles.label}
+          htmlFor='contact__textarea'
+        >
+          How can we help you?
+        </label>
 
-				<textarea
-					type="text"
-					className={`${Styles.input} ${Styles.textarea}`}
-					name="contact__textarea"
-					placeholder="Type here..."
-					onChange={inputHandleChange}
-					value={input.contact__textarea}
-					required
-				/>
-			</div>
+        <textarea
+          type='text'
+          className={`${Styles.input} ${Styles.textarea}`}
+          name='contact__textarea'
+          placeholder='Type here...'
+          onChange={inputHandleChange}
+          value={input.contact__textarea}
+          required
+        />
+      </div>
 
-			<button
-				type="submit"
-				className={`${Styles.submit} ${Styles.btn} btn btn--outline btn--large ${
-					responseFromContactBox.isLoading || responseFromContactBox.data
-						? "btn--complete"
-						: responseFromContactBox.error
-						? "btn--error"
-						: ""
-				}`}
-				disabled={responseFromContactBox.isLoading}
-			>
-				{responseFromContactBox.isLoading || responseFromContactBox.data
-					? "Sent!"
-					: responseFromContactBox.error
-					? "Oops, try again!"
-					: "Send Email"}
-			</button>
+      <button
+        type='submit'
+        className={`${Styles.submit} ${
+          Styles.btn
+        } btn btn--outline btn--large ${
+          responseFromContactBox.isLoading || responseFromContactBox.data
+            ? 'btn--complete'
+            : responseFromContactBox.error
+            ? 'btn--error'
+            : ''
+        }`}
+        disabled={responseFromContactBox.isLoading}
+      >
+        {responseFromContactBox.isLoading || responseFromContactBox.data
+          ? SEND_BUTTON_MSG.SUCCESS
+          : responseFromContactBox.error
+          ? SEND_BUTTON_MSG.ERROR
+          : SEND_BUTTON_MSG.DEFAULT}
+      </button>
 
-			<p
-				className={`${Styles.notification} ${
-					responseFromContactBox.error
-						? `${Styles.show} ${Styles.error}`
-						: responseFromContactBox.data
-						? `${Styles.show} ${Styles.success}`
-						: ""
-				}`}
-			>
-				{responseFromContactBox.data
-					? "Message sent! A copy will be sent to your provided email address."
-					: responseFromContactBox.error
-					? `Error: ${responseFromContactBox.error.message}`
-					: ""}
-			</p>
-		</form>
-	);
+      <p
+        className={`${Styles.notification} ${
+          responseFromContactBox.error
+            ? `${Styles.show} ${Styles.error}`
+            : responseFromContactBox.data
+            ? `${Styles.show} ${Styles.success}`
+            : ''
+        }`}
+      >
+        {responseFromContactBox.data
+          ? RESPONSE_MSG.SUCCESS
+          : responseFromContactBox.error
+          ? responseFromContactBox.error.message
+            ? `Something went wrong. ${responseFromContactBox.error.message}`
+            : RESPONSE_MSG.ERROR
+          : RESPONSE_MSG.DEFAULT}
+      </p>
+    </form>
+  );
 };
 
 export default Contact;
