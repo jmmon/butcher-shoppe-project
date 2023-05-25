c63f160d1bbe47cc51099d67eeb6274ea2186289
jmmon <jmmon93@gmail.com>
2023-05-25 10:37:10 -0700

    format; extra props for dynamic use; cursor pointer separate from styles

--- preact-client/src/components/LinkScrollUp/LinkScrollUp.jsx
+++ preact-client/src/components/LinkScrollUp/LinkScrollUp.jsx
@@ -1,41 +1,57 @@
-import React from "react";
-import { useLocation } from "wouter";
-import LinkWithPreload from "components/LinkWithPreload/LinkWithPreload";
-import LinksStyles from "assets/styles/Links.module.css";
+import React from 'react';
+import { Link, useLocation } from 'wouter';
+import LinkWithPreload from 'components/LinkWithPreload/LinkWithPreload';
+import LinksStyles from 'assets/styles/Links.module.css';
 
-export default function LinkScrollUp({ 
-	className = '', 
-	path, 
-	underline,
-	children 
+export default function LinkScrollUp({
+  className = '',
+  path,
+  underline,
+  children,
+  preload = true,
+  useDefaultClasses = true,
 }) {
-	const [location, ] = useLocation();
-	const scrollToTop = () => {
-		window.scrollTo(0, 0);
-	};
+  const [location] = useLocation();
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
 
-	const isPathMatching = location === path || path === undefined;
+  const isPathMatching = location === path || path === undefined;
 
-	const classes = `${LinksStyles.main_link} ${underline && LinksStyles.underline} ${className}`;
+  const classes = !useDefaultClasses ? className : `${LinksStyles.main_link} ${
+    underline && LinksStyles.underline
+  } ${className}`;
 
-	return (
-		<>
-			{isPathMatching ? (
-				<span
-					tabIndex="0" // not tab-focusable
-					className={classes}
-					onClick={() => scrollToTop()}
-				>
-					{children}
-				</span>
-			) : (
-				<LinkWithPreload 
-					className={classes} 
-					href={path}
-				>
-					{children}
-				</LinkWithPreload>
-			)}
-		</>
-	);
+  React.useEffect(() => {
+    console.log('LinkScrollUp useEffect:', {isPathMatching, preload})
+  }, [])
+
+  return (
+    <>
+      {isPathMatching ? (
+        <span
+          tabIndex='0' // not tab-focusable
+          className={classes}
+          style={{ cursor: 'pointer' }}
+          onClick={() => scrollToTop()}
+        >
+          {children}
+        </span>
+      ) : preload ? (
+        <LinkWithPreload
+          className={classes}
+          href={path}
+        >
+          {children}
+        </LinkWithPreload>
+      ) : (
+        <Link
+          className={classes}
+          to={path}
+        >
+          {children}
+        </Link>
+      )}
+    </>
+  );
 }
