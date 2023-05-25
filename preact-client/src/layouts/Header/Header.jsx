d1ce88d2ebfd249fda393c00c9b3e7d5de738941
jmmon <jmmon93@gmail.com>
2023-05-25 10:38:31 -0700

    make newsleter link scroll up when on same page

--- preact-client/src/layouts/Header/Header.jsx
+++ preact-client/src/layouts/Header/Header.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
-import LinkWithPreload from 'components/LinkWithPreload/LinkWithPreload';
 import TelLink from 'components/TelLink/TelLink';
 import Styles from './Header.module.css';
 
-//import { ReactComponent as CalendarIcon } from "assets/icons/211703_calendar_icon.svg";
+
 import { ReactComponent as PhoneIcon } from 'assets/icons/211829_telephone_icon.svg';
 import { ReactComponent as ChatIcon } from 'assets/icons/211711_chatbubble_icon.svg';
 import { ReactComponent as LocationIcon } from 'assets/icons/211766_location_icon.svg';
 import { ReactComponent as NewsletterIcon } from 'assets/icons/newsletter_icon_64_shrunk_vecta.svg';
+import LinkScrollUp from 'components/LinkScrollUp/LinkScrollUp';
 
 function Header() {
   return (
     <div className={Styles.main}>
       <div className={`flex-jbetween gap-1 ${Styles.container}`}>
-        <LinkWithPreload
+        <LinkScrollUp
           className={`flex-acenter ${Styles.link}`}
-          href='/newsletter/subscribe'
+          path='/newsletter/subscribe'
+          preload={false}
+          useDefaultClasses={false}
         >
           <NewsletterIcon
             className={`${Styles.icon} ${Styles.newsletter_icon}`}
           />
           <span className={Styles.text}>Newsletter</span>
-        </LinkWithPreload>
+        </LinkScrollUp>
 
         <TelLink className={`flex-acenter ${Styles.link}`}>
           <PhoneIcon className={Styles.icon} />
