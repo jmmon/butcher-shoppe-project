80db61bd30482fedfe7b4edb500efe5bcc1cfd76
jmmon <jmmon93@gmail.com>
2023-05-25 10:36:34 -0700

    heading prop; conditional render buttons

--- preact-client/src/layouts/NavBottomButtons/NavBottomButtons.jsx
+++ preact-client/src/layouts/NavBottomButtons/NavBottomButtons.jsx
@@ -1,28 +1,33 @@
-import React from "react";
-import Button from "components/Button/Button";
-import WhitePageBackground from "layouts/WhitePageBackground/WhitePageBackground";
+import React from 'react';
+import Button from 'components/Button/Button';
+import WhitePageBackground from 'layouts/WhitePageBackground/WhitePageBackground';
 
-import Styles from "./NavBottomButtons.module.css";
+import Styles from './NavBottomButtons.module.css';
 
-export default function NavBottomButtons({ prev, next }) {
-	return (
-		<WhitePageBackground separate={true}>
-			<div className={`flex-jevenly-acenter-wrap ${Styles.container}`}>
-				<h4 className={`text-center ${Styles.heading}`}>Helpful Links:</h4>
-				<Button
-					className="btn--outline btn--large"
-					url={prev.link}
-				>
-					{prev.title}
-				</Button>
+export default function NavBottomButtons({ prev, next, heading = "Helpful Links:"}) {
+  return (
+    <WhitePageBackground separate={true}>
+      <div className={`flex-jevenly-acenter-wrap ${Styles.container}`}>
+        <h4 className={`text-center ${Styles.heading}`}>{heading}</h4>
 
-				<Button
-					className="btn--outline btn--large"
-					url={next.link}
-				>
-					{next.title}
-				</Button>
-			</div>
-		</WhitePageBackground>
-	);
+        {prev && (
+          <Button
+            className='btn--outline btn--large'
+            url={prev.link}
+          >
+            {prev.title}
+          </Button>
+        )}
+
+        {next && (
+          <Button
+            className='btn--outline btn--large'
+            url={next.link}
+          >
+            {next.title}
+          </Button>
+        )}
+      </div>
+    </WhitePageBackground>
+  );
 }
